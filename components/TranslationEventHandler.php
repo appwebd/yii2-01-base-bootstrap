7ed4c6e42cfcd8784e21e964192535297e4dd757
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2018-10-31 23:00:15 -0300

    Message is ordered for unrealized translations.

--- components/TranslationEventHandler.php
+++ components/TranslationEventHandler.php
@@ -20,6 +20,6 @@ class TranslationEventHandler
 {
     public static function handleMissingTranslation(MissingTranslationEvent $event)
     {
-        $event->translatedMessage = "@MISSING: {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @";
+        $event->translatedMessage = "@MISSING: FOR LANGUAGE {$event->language}  {$event->category}, {$event->message} @";
     }
 }
