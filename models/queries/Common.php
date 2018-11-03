34c771434dc94f95131a0ef27ac88995933a0cda
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2018-11-02 21:25:33 -0300

    We add a more structured method for transaction operations (delete and save)

--- models/queries/Common.php
+++ models/queries/Common.php
@@ -11,45 +11,113 @@
   * @version     1.0
 */
 
-
 namespace app\models\queries;
 
 use yii;
 use yii\db\ActiveQuery;
 use yii\db\Query;
-use yii\helpers\ArrayHelper;
-use app\models\Controllers;
+use yii\db\Exception;
+use app\controllers\BaseController;
 use app\models\Action;
+use app\models\Controllers;
+use app\models\Permission;
 
 class Common extends ActiveQuery
 {
+    const DENY_ACCESS = 0;
+    const PERMIT_ACCESS = 1;
+    const PROFILE_ID_ADMINISTRATOR = 99;
+    const PROFILE_ID_VISIT = 0;
+    const DONT_REMOVE= 1;
+    const ROWS_ZERO = 0;
+    const ROWS_ONE = 1;
 
+
+    /**
+     * @param $dateInitial string in format YYYY-mm-dd H:i:s
+     * @return string Get date time
+     */
+    public static function getDateDiffNow($dateInitial)
+    {
+
+        try {
+            $now = Common::getNow();
+
+            $return = 10000; // default any value greather than zero
+            $differenceMinutes= (strtotime($now) - strtotime($dateInitial))/60; // answer in minutes
+            if (isset($differenceMinutes)) {
+                $return = $differenceMinutes;
+            }
+            return $return;
+        } catch (Exception $exception) {
+            BaseController::bitacora(
+                Yii::t(
+                    'app',
+                    ERROR_MODULE,
+                    [MODULE => 'getDateDiffNow', ERROR => $exception]
+                ),
+                MSG_ERROR
+            );
+        }
+        return 10000; // default any value greather than zero
+    }
+
+    /**
+     * @param $table string name of table
+     * @param $column string name of column
+     * @param $field string name of column
+     * @param $value  string value of column to compare
+     * @return string description value
+     */
     public static function getDescription($table, $column, $field, $value)
     {
-        $result = ((new Query())->select($column)
-            ->from($table)
-            ->where([$field => $value])
-            ->limit(1)->createCommand())->queryColumn();
-
         $return = '';
-        if (isset($result[0])) {
-            $return = $result[0];
+        try {
+            $result = ((new Query())->select($column)
+                ->from($table)
+                ->where([$field => $value])
+                ->limit(1)->createCommand())->queryColumn();
+            $return = '';
+            if (isset($result[0])) {
+                $return = $result[0];
+            }
+        } catch (Exception $errorexception) {
+            BaseController::bitacora(
+                Yii::t(
+                    'app',
+                    ERROR_MODULE,
+                    [MODULE => '@app\models\queries\getDescription', ERROR => $errorexception]
+                ),
+                MSG_ERROR
+            );
         }
+
+
         return $return;
     }
 
+    /**
+     * @param $table string table name
+     * @return int value of count nro of records in table
+     */
     public static function getNroRows($table)
     {
 
         $count = (new Query())->select('COUNT(*)')->from($table) ->limit(1);
 
-        $return = 0;
+        $return = Common::ROWS_ZERO;
         if (isset($count)) {
             $return = $count->count();
         }
         return $return;
     }
 
+    /**
+     * @param $table string name of table in database
+     * @param $field string column name in table
+     * @param $value string value
+     * @return int numbers of rows in other tables with integrity referential found.
+     */
     public static function getNroRowsForeignkey($table, $field, $value)
     {
         $count = (new Query())->select('count(*)')
@@ -57,7 +125,7 @@ class Common extends ActiveQuery
             ->where(["$field" => $value])
             ->limit(1);
 
-        $return = 0;
+        $return = Common::ROWS_ZERO;
         if (isset($count)) {
             $return = $count->count();
         }
@@ -73,7 +141,7 @@ class Common extends ActiveQuery
         $result = ((new Query())->select('now()')
             ->limit(1)->createCommand())->queryColumn();
 
-        $return = '';
+        $return = date(DATEFORMAT);
         if (isset($result[0])) {
             $return = $result[0];
         }
@@ -82,57 +150,42 @@ class Common extends ActiveQuery
 
     /**
      * Check user permission for any resources like tables (Controllers/Action)
-     * @param $action_name
+     * @param $actionName
+     * @return int grant or deny access
+     * @throws Exception
      */
     public static function getProfilePermission($actionName)
     {
         if (isset(Yii::$app->user->identity->profile->profile_id)) {
             $profileId = Yii::$app->user->identity->profile->profile_id;
         } else {
-            $profileId = 0;
+            $profileId = Common::PROFILE_ID_VISIT;
         }
 
-        if ($profileId==99) {  // 99=profile administrator
-            return 1;          // OK = 1 = Permit access
+        if ($profileId==Common::PROFILE_ID_ADMINISTRATOR) {
+            return Common::PERMIT_ACCESS;
         }
 
         $controllerName    = Yii::$app->controller->id;  // controller name
+        $controllerId = Controllers::getControllerId($controllerName);
+        $actionId = Action::getActionId($actionName);
 
-        $controllerId = ((new Query())->select('controller_id')
-            ->from('controllers')
-            ->where(["controller_name"=>$controllerName])
-            ->limit(1)->createCommand())->queryColumn();
-
-        $actionId = ((new Query())->select('action_id')
-            ->from('action')
-            ->where(["action_name"=>$actionName])
-            ->limit(1)->createCommand())->queryColumn();
-
-
-        $actionPermission = 0;
-        if (isset($controllerId[0]) && isset($actionId[0])) {
-            $actionPermission = Yii::$app->db->createCommand(
-                "SELECT action_permission
-                     FROM permission
-                     WHERE action_id=".$actionId[0]."
-                   and controller_id=".$controllerId[0] . " and profile_id=". $profileId
-            )->queryOne();
-
-            if (!isset($actionPermission) || $actionPermission===false) {
-                $actionPermission=0;
-            }
+        $actionPermission = Common::DENY_ACCESS;
+        if (isset($controllerId) && isset($actionId)) {
+            $actionPermission = Permission::getPermission($actionId, $controllerId, $profileId);
         }
 
         return $actionPermission;
     }
 
     /**
-     * @param $showButtons
+     * @param $showButtons string what buttons should show in the view
      * @return string
+     * @throws Exception
      */
-    public static function getProfilePermissionString($showButtons='111')
+    public static function getProfilePermissionString($showButtons = '111')
     {
-        $aButton = str_split($showButtons,1);
+        $aButton = str_split($showButtons, 1);
 
         $template = '';
         if ($aButton[0] && Common::getProfilePermission('view')) {
@@ -147,5 +200,60 @@ class Common extends ActiveQuery
             $template .=' {delete}';
         }
         return $template;
+    }
+
+    /**
+     * @param $model mixed class defined in @app\models\
+     * @param $method
+     * @return bool Success o failed to create/update a $model in this view
+     * @throws yii\db\Exception Failed to save a record error: {error}
+     */
+    public static function transaction(&$model, $method)
+    {
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            if ($model->$method()) {
+                $transaction->commit();
+                return true;
+            }
+            $transaction->rollBack();
+        } catch (\Exception $exception) {
+            BaseController::bitacoraAndFlash(
+                Yii::t(
+                    'app',
+                    'Failed to {method} record error: {error}',
+                    ['method' => $method, ERROR=> $exception]
+                ),
+                MSG_ERROR
+            );
+            $transaction->rollBack();
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $sqlcode string sql instruction
+     * @return bool int true/false answer: query was executed with errors?
+     * @throws Exception
+     */
+    public static function sqlCreateCommand($sqlcode)
+    {
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            if (Yii::$app->db->createCommand($sqlcode)->execute()) {
+                $transaction->commit();
+                return true;
+            }
+            $transaction->rollBack();
+            return false;
+        } catch (\Exception $exception) {
+            BaseController::bitacoraAndFlash(
+                Yii::t('app', 'Failed execute SQL: {error}', [ERROR => $exception]),
+                MSG_ERROR
+            );
+            $transaction->rollBack();
+        }
+        return false;
     }
 }
