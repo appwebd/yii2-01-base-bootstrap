01e8d39d930c4dcb4057a4675226b7c36678ef87
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2018-11-02 21:08:55 -0300

    add PHPDoc weak warning fixed

--- components/TranslationEventHandler.php
+++ components/TranslationEventHandler.php
@@ -16,10 +16,17 @@ namespace app\components;
 
 use yii\i18n\MissingTranslationEvent;
 
+/**
+ * Class TranslationEventHandler
+ * @package app\components
+ */
 class TranslationEventHandler
 {
+    /**
+     * @param MissingTranslationEvent $event
+     */
     public static function handleMissingTranslation(MissingTranslationEvent $event)
     {
-        $event->translatedMessage = "@MISSING: FOR LANGUAGE {$event->language}  {$event->category}, {$event->message} @";
+        $event->translatedMessage = "@MISSING: FOR LANGUAGE {$event->language} {$event->category}, {$event->message} @";
     }
 }
