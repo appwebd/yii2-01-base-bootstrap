dbb38a9aa3996aeafa1556ce23a1963363bd7f51
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2019-02-27 19:43:56 -0300

    improve layout intentation

--- views/site/index.php
+++ views/site/index.php
@@ -14,9 +14,14 @@ $this->title = 'My Yii Application';
             <br/>
             <div class="jumbotron">
                 <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra atte</p>
+                <p class="lead">
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra atte
+                </p>
                 <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p>It uses utility classes for typography and spacing to space content out
+                    within the larger container.
+                </p>
                 <p class="lead">
                     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p>
