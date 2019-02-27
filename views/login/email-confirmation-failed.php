1bd73aacf9a479ce0c67b96dc43ad747ab9ca979
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2019-02-27 20:52:50 -0300

    Improvement in documentation of code

--- views/login/email-confirmation-failed.php
+++ views/login/email-confirmation-failed.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Company
+ * email confirmation failed view
  *
- * @package     form of Company
+ * @package     login
  * @author      Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
  * @copyright   (C) Copyright - Web Application development
  * @license     Private license
@@ -11,11 +11,7 @@
  * @version     1.0
  */
 
-use yii\web\View;
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
 use app\components\UiComponent;
-use app\models\forms\PasswordResetForm;
 
 $this->title = Yii::t('app', 'Email confirmation');
 $this->params[BREADCRUMBS][] = $this->title;
@@ -23,7 +19,7 @@ $this->params[BREADCRUMBS][] = $this->title;
 echo '
 <div class="container">
     <div class="row">
-        <div class="col-sm-3 "> &nbsp; </div>        
+        <div class="col-sm-3 "> &nbsp; </div>
         <div class="col-sm-6 box">
 
             <div class="webpage">';
