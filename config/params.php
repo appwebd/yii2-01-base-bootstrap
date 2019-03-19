5f8f78373abb9101c7e179b58e7ed46773bd9dcf
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2019-03-19 16:06:34 -0300

    Code cleanup, rearrange code, optimization of import

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'adminEmail' => 'pro@dev-client.local',
-    'pageSizeDefault'=>'5',
+    'pageSizeDefault' => '5',
 
     'user.passwordResetTokenExpire' => 3600,
     'user.emailConfirmationTokenExpire' => 43200, // 5 days
