2062357e00406848cfeffa26341cdbd828b34c47
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2020-02-01 20:56:27 -0300

    Was added register any error to database.

--- dist/helpers/Mail.php
+++ dist/helpers/Mail.php
@@ -1,8 +1,22 @@
 <?php
+/**
+ * Class Mail
+ * PHP Version 7.2
+ *
+ * @category  Helpers
+ * @package   Mail
+ * @author    Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
+ * @copyright 2019 Copyright - Web Application development
+ * @license   Private license
+ * @version   GIT: <git_id>
+ * @link      https://appwebd.github.io
+ * @date      29/10/2019 12:13 PM
+ */
 namespace app\helpers;
 
 use Yii;
 use Swift_SwiftException;
+use app\models\queries\Bitacora;
 
 /**
  * A helper class for sending mails
@@ -19,15 +33,17 @@ class Mail
      * The sender can be specified in `$params['from']` and defaults to the
      * `mail.from` application parameter.
      *
-     * @param $user object key of table user
-     * @param $subject string subject of email
-     * @param string $view name of the view file to render from `@app/mail/user`
+     * @param object $user    key of table user
+     * @param string $subject subject of email
+     * @param string $view    name of the view file to render from `@app/mail/user`
+     *
      * @return bool whether the mail was sent successfully
      */
     public static function sendEmail($user, $subject, $view)
     {
 
         try {
+            $bitacora = new Bitacora();
             $from   = Yii::$app->params['adminEmail'];
             $sendOK = Yii::$app
                 ->mailer
@@ -42,20 +58,38 @@ class Mail
                 ->setSubject($subject)
                 ->send();
 
-            if ($sendOK) {
-                $msg= Yii::t('app', 'An email has been sent to your email account');
-                Yii::info($msg, __METHOD__);
-                return true;
-            } else {
-                $msg= Yii::t('app', 'The sending of emails to your email account has failed:'. $user->email);
-                Yii::warning($msg, __METHOD__);
-                return false;
+            $message = Yii::t(
+                'app',
+                'Subscription: Successful mail delivery:'. $user->email
+            );
+            $statusID = \MSG_SUCCESS;
+            if (!$sendOK) {
+                $message = Yii::t(
+                    'app',
+                    'Subscription mail delivery failed:'. $user->email
+                );
+                $statusID = \MSG_ERROR;
+                Yii::warning($message, __METHOD__); // for debug purposes
             }
-        } catch (Swift_SwiftException $e) {
-            $type = get_class($e);
-            $message = $e->getMessage();
-            $trace = $e->getTraceAsString();
-            Yii::warning("Swift exception $type:\n$message\n\n$trace", __METHOD__);
+            $bitacora->register(
+                $message,
+                'app/helpers/Mail::sendEmail',
+                $statusID
+            );
+            return $sendOK;
+        } catch (Swift_SwiftException $exception) {
+            $type = get_class($exception);
+            $message = $exception->getMessage();
+            $trace = $exception->getTraceAsString();
+            $message = "Swift exception $type:\n$message\n\n$trace";
+            Yii::warning($message, __METHOD__); // for debug purposes
+
+            $bitacora = new Bitacora();
+            $bitacora->register(
+                $message,
+                'app/helpers/Mail::sendEmail catch',
+                MSG_ERROR
+            );
         }
 
         return false;
